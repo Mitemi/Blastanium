6608d91ef010b0ebeb963893d85de9a4e701b2d8
Miter365 <miter360@gmail.com>
2024-08-04 18:32:27 -0400

    VII

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const blastanium = Item("compound")
 
-// Vanilla Turrets Compatibility
+// Vanilla Serpulo Turrets Compatibility
 const compoundBulletSpectre = extend(FlakBulletType, {
     speed: 4,
     lifetime: 55,
@@ -76,7 +76,7 @@ const compoundBulletCyclone = extend(FlakBulletType, {
     })
 });
 
-const compoundBulletRipple = extend(FlakBulletType, {
+const compoundBulletRipple = extend(ArtilleryBulletType, {
     speed: 4,
     lifetime: 65,
     damage: 65,
@@ -101,7 +101,7 @@ const compoundBulletRipple = extend(FlakBulletType, {
     })
 });
 
-const compoundBulletSwarmer = extend(FlakBulletType, {
+const compoundBulletSwarmer = extend(MissileBulletType, {
     speed: 4,
     lifetime: 55,
     damage: 15,
@@ -132,3 +132,77 @@ Blocks.ripple.ammoTypes.put(Vars.content.item("blastanium-compound"), compoundBu
 Blocks.cyclone.ammoTypes.put(Vars.content.item("blastanium-compound"), compoundBulletCyclone);
 Blocks.spectre.ammoTypes.put(Vars.content.item("blastanium-compound"), compoundBulletSpectre);
 Blocks.swarmer.ammoTypes.put(Vars.content.item("blastanium-compound"), compoundBulletSwarmer);
+
+// Vanilla Erekir Turrets Compatibility
+
+const compoundBulletScathe = extend(ArtilleryBulletType, {
+    speed: 4,
+    lifetime: 300,
+    Damage: 1000,
+    shootEffect: Fx.shootTitan,
+    ammoMultiplier: 2,
+    splashDamage: 2900,
+    splashDamageRadius: 75,
+    collidesGround: true,
+    status: StatusEffects.blasted,
+    statusDuration: 120,
+    hitSound: Sounds.titanExplosion,
+    fragBullet: extend(BasicBulletType, {
+        speed: 2.5,
+        damage: 170,
+        sprite: "bullet",
+        width: 10,
+        height: 12,
+        shrinkY: 1,
+        lifetime: 20,
+        backColor: Color.valueOf("7f7f7f"),
+        frontColor: Color.valueOf("c8ff00"),
+        despawnEffect: Fx.none,
+        buildingDamageMultiplier: 0.35
+    }),
+    buildingDamageMultiplier: 0.35
+});
+
+const compoundBulletTitan = extend(ArtilleryBulletType, {
+    speed: 4,
+    lifetime: 95,
+    damage: 0,
+    shootEffect: Fx.shootTitan,
+    ammoMultiplier: 2,
+    splashDamage: 650,
+    splashDamageRadius: 75,
+    collidesGround: true,
+    status: StatusEffects.blasted,
+    statusDuration: 120,
+    width: 22,
+    height: 26,
+    scaledSplashDamage: true,
+    trailLength: 32,
+    trailWidth: 3.35,
+    trailSinScl: 2.5,
+    trailSinMag: 0.5,
+    trailEffect: Fx.none,
+    despawnShake: 7,
+    knockback: 2,
+    hitSound: Sounds.titanExplosion,
+    hitEffect: MultiEffect(Fx.titanExplosion, Fx.titanSmoke),
+    fragBullet: extend(BasicBulletType, {
+        speed: 2.5,
+        damage: 5,
+        sprite: "bullet",
+        width: 10,
+        height: 12,
+        shrinkY: 1,
+        lifetime: 20,
+        splashDamage: 50000,
+        splashDamageRadius: 75,
+        backColor: Color.valueOf("7f7f7f"),
+        frontColor: Color.valueOf("c8ff00"),
+        despawnEffect: Fx.none,
+        buildingDamageMultiplier: 0.35
+    }),
+    buildingDamageMultiplier: 0.35
+});
+
+Blocks.scathe.ammoTypes.put(Vars.content.item("blastanium-compound"), compoundBulletScathe);
+Blocks.titan.ammoTypes.put(Vars.content.item("blastanium-compound"), compoundBulletTitan);
